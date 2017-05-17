cd8972c1e2d933f6aa05145f96a819340856ac2d
Monica Powell <monica@jopwell.com>
2017-05-17 19:04:03 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+/// telling the dom to render the MamaContainer from the root
+import ReactDOM from 'react-dom';
+import MamaContainer from './MamaContainer';
+
+
+
+
+ReactDOM.render(<MamaContainer />, document.getElementById('root'));
---
+++ src/listItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class listItem extends Component {
+
+ render() {
+//list item with check box - can be marked as completed
+
+
+// <   form action="">
+// <input type="checkbox" name="vehicle" value="Bike">I have a bike<br>
+// <input type="checkbox" name="vehicle" value="Car">I have a car
+// </form>
+
+ }
+}
