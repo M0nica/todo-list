e614bc3e752aa569ed62d02164b3c1179cdc224c
Monica Powell <monica@jopwell.com>
2017-05-17 21:09:14 -0400

    todo list app is working

--- src/List.js
+++ src/List.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import ListItem from './ListItem';
 
 export default class List extends Component {
-
-render(){
-  return (
-    <div>
-    
-    </div>
-  );
-}
+  render() {
+    const { items, removeItemFromList } = this.props;
+    return (
+      <div>
+        <ul>
+          {
+            items.map(
+              (item, index) => {
+                return <ListItem
+                  key={index}
+                  item={item}
+                  index={index}
+                  removeItem={removeItemFromList}
+                />
+              }
+            )
+          }
+        </ul>
+      </div>
+    );
+  }
 }
--- src/TodoContainer.js
+++ src/TodoContainer.js
@@ -3,15 +3,45 @@ import React, { Component } from 'react';
 import AddItem from './AddItem';
 import List from './List';
 
+let initialItems = [
+    "Get milk from the store",
+    "Call my mom...",
+    "Build the internet",
+    "Slim Jims and Diet Coke",
+    "Bake Christmas Cookies For NYTimes Party",
+];
+
 export default class TodoContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { items: initialItems }
+   this.addItemToList = this.addItemToList.bind(this)
+   this.removeItemFromList = this.removeItemFromList.bind(this)
+    }
+
+    removeItemFromList(item){
+    initialItems.splice(item, 1);
+
+    //sends updated list to component and has it re-renders
+    this.setState({ items: initialItems });
+}
+
+addItemToList(){
+    let newItem = document.getElementById('new-item').value;
+    initialItems.push(newItem)
+    this.setState({ items: initialItems });
+    document.getElementById('new-item').value = '';
+}
 
  render() {
 
    return (
    <div className = "container">
     <h1>Awesome To-Do List</h1>
-    < List />
-    <AddItem />
+      <AddItem addItemToList={this.addItemToList}/>
+      <List items={this.state.items} removeItemFromList={this.removeItemFromList} />
+
 
    </div>
 )
